304c0b86632e89b4286c1867e40cb45f9efa6149
blackwarriorxtn <blackwarriorxtn99@gmail.com>
2021-01-18 22:11:24 +0100

    Updates

---
+++ precache-manifest.20a3581e08b0bae22bc1628f44090ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e7703de32a094d79e9ab06e3e0a931",
+    "url": "/LevelUP-Club.github.io/index.html"
+  },
+  {
+    "revision": "27826d92b52d5fe6c869",
+    "url": "/LevelUP-Club.github.io/static/css/main.c7bb4aff.chunk.css"
+  },
+  {
+    "revision": "677e477ecd6aba6f53ab",
+    "url": "/LevelUP-Club.github.io/static/js/2.d3b2bad9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/LevelUP-Club.github.io/static/js/2.d3b2bad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27826d92b52d5fe6c869",
+    "url": "/LevelUP-Club.github.io/static/js/main.62696d52.chunk.js"
+  },
+  {
+    "revision": "c25088059852697e8ef2",
+    "url": "/LevelUP-Club.github.io/static/js/runtime-main.295699a1.js"
+  },
+  {
+    "revision": "8599e5328d925ae05e32b41d5a112bfb",
+    "url": "/LevelUP-Club.github.io/static/media/logolevelup.8599e532.png"
+  }
+]);
